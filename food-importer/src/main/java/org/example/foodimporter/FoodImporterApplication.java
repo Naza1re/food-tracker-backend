d87cdc12947ac33e0ad3b945bab7ebf422b6ba08
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-04-14 00:07:15 +0300

    Initial commit for feature food-importer

---
+++ food-importer/src/main/java/org/example/foodimporter/FoodImporterApplication.java
@@ -0,0 +1,13 @@
+package org.example.foodimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodImporterApplication.class, args);
+    }
+
+}
---
+++ food-importer/src/test/java/org/example/foodimporter/FoodImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foodimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
